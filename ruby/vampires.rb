252748867e1ff7690ab3c1b7c5b07cf9ecc58553
Woojae Yi <wooj.d.yi_2302@berkeley.edu>
2016-10-07 05:22:13 -0700

    Committing vampires.rb, release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,3 +8,27 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 garlic = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+interview = nil
+if age && (garlic || insurance)
+  interview = "Not"
+if !age && (!garlic || !insurance)
+  interview = "Probably"
+if !age && !garlic && !insurance
+  interview = "Certain"
+if name == "Drake Cula" || name == "Tu Fang"
+  interview = "Definitely"
+else
+  interview
+end
+case interview
+when "Not"
+  print "Probably not a vampire."
+when "Probably"
+  print "Probably a vampire."
+when "Certain"
+  print "Almost certainly a vampire."
+when "Definitely"
+  print "Definitely a vampire."
+when nil
+  print "Results inconclusive."
+end
