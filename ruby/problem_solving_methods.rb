959a7e08f831e4db012d1ae124a93cc6110e2611
oxwootang <oxwootang@users.noreply.github.com>
2016-10-16 23:14:28 -0700

    Update problem_solving_methods.rb

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -18,14 +18,13 @@ end
 # p int_search([0, 1, 2, 5, 6, 10], 3)
 # p int_search([0, 1, 2, 5, 6, 10], 6)
 
-<<<<<<< HEAD
+
 # I chose to research bubble sort. It works visually like bubbles rising to the surface. It sorts by making adjacent comparisons in an array and repeating the process until all the components are in the correct order in the array.
 # The visualization in http://www.algolist.net/Algorithms/Sorting/Bubble_sort helped me understand it immediately.
 # I felt excited and curious because it was cool to find a (although inefficient-order of n^2) method that works so logically.
 # Def sorting_method (array of integers)
 #   to sort in decreasing order, check index 0 and 1 of the array for the greater integer; if index 0 > 1 (the correct order), move on to check index 1 and 2, and so on until the first sweep is done. then repeat until every number is in order.
 
-# My bubble_sort search method doesn't work yet. I'm working on finding a solution to how the method can know if all the elements are properly sorted. A boolean would store this variable, and when it is true it would be done sorting. But I don't know when/where/how in my code to implement an algorithm that would check if the list was done.
 
 def bubble_sort(int_array)
   to_return = int_array
@@ -42,21 +41,20 @@ def bubble_sort(int_array)
         count += 1
       end
     end
-=======
+
 def fib(terms)
   start = 2
   to_return = [0, 1]
   while start < terms
     to_return << to_return[start-1] + to_return[start-2]
     start += 1
->>>>>>> 911f7235f01a1d1293492ed9bda395795e0285a2
   end
   return to_return
 end
 
-<<<<<<< HEAD
+
 p bubble_sort([1,5,6,3])
-=======
+
 p fib(6)
 p fib(100)[-1]
->>>>>>> 911f7235f01a1d1293492ed9bda395795e0285a2
+
