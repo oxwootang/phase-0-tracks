149ec10a2f93dbfc73834eb017459706fc219ee5
Woojae Yi <wooj.d.yi_2302@berkeley.edu>
2016-10-20 12:22:12 -0700

    Committing puppy_methods.rb, added boxer class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -23,6 +23,10 @@ class Puppy
     puts "*lies down*"
   end
 
+  def initialize
+    puts "Initializing new Puppy instance ..."
+  end
+
 end
 
 pup = Puppy.new
@@ -31,3 +35,31 @@ pup.speak(3)
 pup.roll_over
 pup.dog_years(4)
 pup.lay_down
+
+class Boxers
+
+  def initialize
+    puts "Initializing new boxer instance."
+  end
+
+  def jab(hand)
+    puts "Punch with your #{hand} hand!"
+  end
+
+  def roundhouse_kick(leg)
+    puts "Roundhouse kick with your #{leg} leg!"
+  end
+
+end
+
+boxing_team = []
+until boxing_team.length > 50
+  boxer = Boxers.new
+  boxing_team << boxer
+  puts boxing_team
+end
+
+boxing_team.each do |box|
+  puts "#{box.jab("right")}"
+  puts "#{box.roundhouse_kick("left")}"
+end
