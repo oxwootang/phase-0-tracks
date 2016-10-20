750c3a891c8e8286624f4a7ab056fe3aa873cd35
Woojae Yi <wooj.d.yi_2302@berkeley.edu>
2016-10-20 12:07:29 -0700

    Committing puppy_methods.rb

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    num.times do
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    puts human_years*7
+  end
+
+  def lay_down
+    puts "*lies down*"
+  end
+
 end
 
+pup = Puppy.new
+pup.fetch("ball")
+pup.speak(3)
+pup.roll_over
+pup.dog_years(4)
+pup.lay_down
