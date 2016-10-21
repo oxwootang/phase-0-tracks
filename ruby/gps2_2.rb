e7e18e9bc41f075f491fe9f03000e909a52c48fc
Woojae Yi <wooj.d.yi_2302@berkeley.edu>
2016-10-20 21:10:35 -0700

    Committing gps2_2.rb, adding methods to update, remove, add, create, and print out grocery lists using hashes

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,27 +1,71 @@
-# Method to create a list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps:
-  # [fill in any steps here]
-  # set default quantity
-  # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
-
 # Method to add an item to a list
 # input: item name and optional quantity
+# steps: initialize a hash with the grocery list as a list of keys.
+# iterate through array to update hash.
+# output: return a string that tells what changed.
+def add_item(our_list, item_name, quantity = 1)
+  our_list[item_name] = quantity
+  puts our_list
+end
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# output: [array]
 # steps:
-# output:
+  # use string methods to split the string input into an array and make the hash using the list as keys
+  # print the hash to the console [use the method to print a list]
+def grocery_list(item_string)
+  items = item_string.split(" ")
+  grocery = {}
+  for item in items do
+    add_item(grocery, item)
+  end
+  grocery
+end
+
+our_list = grocery_list("apple oranges cereal pizza")
+# my_list = grocery_list(...)
+# your_list = grocery_list(..)
+
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: item name
+# steps: search for the item name in the list.
+# delete the item from the list.
+# output: return a string that tells what changed.
+def remove_item(our_list, item_name)
+  our_list.delete_if {|key, value| key == item_name}
+  puts our_list
+end
+
+# remove_item(our_list, "apple")
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: item name, new quantity
+# steps: assign new value to item key in hash
+# output: return a string that tells what changed
+def update(our_list, item_name, quantity)
+  our_list[item_name] = quantity
+  puts our_list
+end
+
+# update(our_list, "oranges", 4)
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: hash
+# steps: puts hash with #each using |key, value| for block parameter
+# output: print string of hash
+
+def print_list(our_list)
+  our_list.each do |key, value|
+    puts "#{key} : #{value}"
+  end
+end
+
+add_item(our_list, "onion")
+add_item(our_list, "lemonade", 2)
+add_item(our_list, "tomatoes", 3)
+add_item(our_list, "ice cream", 4)
+remove_item(our_list, "lemonade")
+update(our_list, "ice cream", 1)
+print_list(our_list)
