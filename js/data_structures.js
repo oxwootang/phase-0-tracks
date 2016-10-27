ebe3a8c083a72ba2953e82203c7d259ebf3c8642
Woojae Yi <wooj.d.yi_2302@berkeley.edu>
2016-10-27 13:17:05 -0700

    Committing data_structures.js, adding car constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,26 @@ for (var i = 0; i < colorArray.length; i++) {
   horseObjects[horseNames[i]] = colorArray[i];
 }
 console.log(horseObjects);
+
+// constructor function for a Car
+function Car(model, mpg, used) {
+  this.model = model;
+  this.mpg = mpg;
+  this.used = used;
+  this.radio = function() { console.log("Playing this month's top hits, at DBC radio!"); };
+}
+
+var myCar = new Car("Honda", 30, false);
+myCar.radio();
+myCar.clean = false;
+myCar.washCar = function() {
+  if (this.clean == false) {
+    console.log("Going to the car wash!");
+    this.clean = true;
+    console.log("My car is sparkly clean!");
+  }
+  else {
+    console.log("My car is sparkly clean!");
+  }
+};
+myCar.washCar();
