4734926305b86ac94d9e8dccfc5394ea83c65091
Woojae Yi <wooj.d.yi_2302@berkeley.edu>
2016-10-31 08:31:02 -0700

    Committing my_solution.rb, refactoring predicted_deaths and speed_of_spread

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -28,18 +28,11 @@ class VirusPredictor
   # this predicted_deaths instance method takes in population_density, population, and state instance attributes to calculate the estimated number_of_deaths.
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
-    if population_density >= 200
-      number_of_deaths = (population * 0.4).floor
-    elsif population_density >= 150
-      number_of_deaths = (population * 0.3).floor
-    elsif population_density >= 100
-      number_of_deaths = (population * 0.2).floor
-    elsif population_density >= 50
-      number_of_deaths = (population * 0.1).floor
-    else
+    if population_density <= 50
       number_of_deaths = (population * 0.05).floor
+    else
+      number_of_deaths = (population * 0.1 * (population_density.floor/50)).floor
     end
-
     print "#{state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -49,19 +42,11 @@ class VirusPredictor
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
-
     if population_density >= 200
-      speed += 0.5
-    elsif population_density >= 150
-      speed += 1
-    elsif population_density >= 100
-      speed += 1.5
-    elsif population_density >= 50
-      speed += 2
+      speed = 0.5
     else
-      speed += 2.5
+      speed = 2.5 - 0.5 * (population_density.floor/50)
     end
-
     puts " and will spread across the state in #{speed} months.\n\n"
 
   end
